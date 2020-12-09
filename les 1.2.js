fabb6d42f126f2cc65806e10f253657448a62397
alina-developer <71496763+alina-developer@users.noreply.github.com>
2020-12-09 13:51:54 +0300

    Create les 1.2.js

---
+++ les 1.2.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать значение из name в admin. Вывести в консоль переменную admin (должно вывести "Василий").*/
+let admin;
+let name;
+name= "Василий";
+admin=name;
+console.log(admin);
